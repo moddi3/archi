2f33c1302b4b948298cd43469a6f42c37a7188c6
moddi3 <vlad211099@gmail.com>
2015-03-26 18:11:16 +0200

    Create system.sh

---
+++ aui/system.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+arch-chroot /mnt /bin/bash
+nano /etc/locale.gen
+locale-gen
+echo LANG=ru_RU.UTF-8 > /etc/locale.conf
+export LANG=ru_RU.UTF-8
+ln -s /usr/share/zoneinfo/Europe/Kiev /etc/localtime
+echo moddi3 > /etc/hostname
+nano /etc/hosts
+sudo NetworkManager
+nmcli dev wifi connect osnova3 password 36376971
+ping -c 3 google.com
+mkinitcpio -p linux
+passwd
+pacman -S grub os-prober
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+echo -e "${YELLOW} Done! "
