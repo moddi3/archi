fd43f5a3273311cf9dc0b6f5a21133a8bc2b87a4
moddi3 <vlad211099@gmail.com>
2015-03-26 18:58:49 +0200

    Create post_install.sh

---
+++ aui/post_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo pacman -S xorg-server xorg-server-utils xorg-xinit xorg-xinput xf86-video-ati xf86-video-intel xf86-input-synaptics xf86-input-keyboard xf86-input-mouse --noconfirm
+cp /etc/skel/.xinitrc ~
+sudo pacman -S xfce4 xfce4-goodies --noconfirm
