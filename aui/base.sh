c41368b5b3fe84db87fd4d3211194013aa07e4b1
moddi3 <vlad211099@gmail.com>
2015-03-26 18:01:32 +0200

    Create base.sh

---
+++ aui/base.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+YELLOW='\033[0;33m'
+echo -e "${YELLOW} KEYMAP "
+loadkeys ru
+echo -e "${YELLOW} FONT "
+setfont cyr-sun16
+nano /etc/locale.gen
+echo -e "${YELLOW} LOCALE.GEN "
+locale-gen
+export LANG=ru_RU.UTF-8
+wifi-menu
+echo -e "${YELLOW} LSBLK "
+lsblk
+mkfs.ext2 -L boot /dev/sda5
+mkfs.ext4 -L arch /dev/sda6
+mkfs.ext4 -L home /dev/sda8
+mkswap -L swap /dev/sda7
+swapon /dev/sda7
+mount /dev/sda6 /mnt
+mkdir -p /mnt/{boot,home}
+mount /dev/sda5 /mnt/boot
+mount /dev/sda8 /mnt/home
+mv /etc/pacman.d/mirrorlist mirrorlist.old
+cp mirrorlist > /etc/pacman.d/mirrorlist
+pacstrap /mnt base base-devel dialog wpa_supplicant networkmanager
+genfstab -U -p /mnt >> /mnt/etc/fstab
+nano /mnt/etc/fstab
+echo -e "${YELLOW} DONE! "
